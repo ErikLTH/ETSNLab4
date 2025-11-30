3e70d8b278fbc3286768f42dc4d83557d9109667
Erik Goch Gustafsson <erik.goch_gustafsson.3618@student.lu.se>
2025-11-30 13:36:16 +0100

    La till bas men enkel string splittning

---
+++ readfile.py
@@ -0,0 +1,42 @@
+
+
+def openfile(file):
+    """Open a file and return its contents as a list of lines."""
+    
+    file = open(file, 'r')
+    lines = file.readlines()
+    file.close()
+
+    return lines
+
+
+def main():
+    try:
+        lines = openfile('nilsholg.txt')
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return
+
+
+    print(lines[0])
+    
+    # for j in lines:
+    #     if j == "\n":
+    #         print("Empty line")
+    #     print(j)
+    #     print("\n")
+
+    counter = 0
+    
+    for line in lines:
+        ord = line.split(' ') # Splittar varje rad i ord.
+        #print(ord)
+        for i in ord:
+            if i.lower() == 'kristna':
+                counter += 1
+                print(i)
+
+    print("Totalt antal ord av: " + str(counter))
+
+if __name__ == '__main__':
+    main()
