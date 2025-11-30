d3c07a964c7f2189f9bb87cce1d7ea9b643e2ea1
Erik Goch Gustafsson <erik.goch_gustafsson.3618@student.lu.se>
2025-11-30 14:23:46 +0100

    final version

--- readfile.py
+++ readfile.py
@@ -1,3 +1,6 @@
+import sys
+import re
+
 
 
 def openfile(file):
@@ -11,24 +14,49 @@ def openfile(file):
 
 
 def main():
+    """Main function to search for a pattern in a file."""
+
+
+    #Atleast 3 arguments are needed
+    if len(sys.argv) < 3:
+        print("Usage: search <pattern> <file>")
+        return
+
+    #Pick out the passed arguments from the CLI
+    pattern = sys.argv[1]
+    filename = sys.argv[2]
+    
+
+    #Try open the file and handle exceptions
     try:
-        lines = openfile('nilsholg.txt')
+        lines = openfile(filename)
     except Exception as e:
         print(f"An error occurred: {e}")
         return
 
-
-    counter = 0
+    is_single_word = len(pattern.split()) == 1
     
     for line in lines:
-        ord = line.split(' ') # Splittar varje rad i ord.
-        #print(ord)
-        for i in ord:
-            if i.lower() == 'kristna':
-                counter += 1
-                print(i)
+        
+        # Split the each line into a list of words, using regex to handle various delimiters such as , . ! ? ; : \n \t and spaces
+        words = re.split(r"[ ,.!?;:\n\t]+", line)
 
-    print("Totalt antal ord av: " + str(counter))
+        # If patterns contains only one word, we check each word in the line.
+        if(is_single_word): 
+            for i in words:
+                if i.lower() == pattern.lower():
+                    print(line)
+
+                    # No need to check further words in the line, no need for duplicate lines
+                    break 
+
+        # If patterns contatins more than one word, we check if the whole line contains the pattern.
+        else:
+            if line.lower().__contains__(pattern.lower()):
+                print(line)
+
+
 
 if __name__ == '__main__':
+    """Run the main function."""
     main()
